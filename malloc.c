ea4e6e40f8369498a3b781e7a80dd35a6e7da79a
Anusha <anushadharemane@gmail.com>
2026-03-13 14:08:39 +0530

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int *arr;
+    int n=5;
+    arr=(int*)malloc(n*sizeof(int));
+    if(arr==NULL){
+        printf("Memory is not allocated");
+        return 1;
+    }
+    for(int i=0;i<n;i++){
+        arr[i]=i*1;
+    }
+    printf("Array elements:");
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+}
+
